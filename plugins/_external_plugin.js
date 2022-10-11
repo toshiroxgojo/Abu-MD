017ae8e78d95797f6703e54d91aba97f17e6499d
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-10-12 05:10:01 +0530

    Update _external_plugin.js

--- plugins/_external_plugin.js
+++ plugins/_external_plugin.js
@@ -4,10 +4,10 @@ const {
 const Config = require('../config');
 const axios = require('axios');
 const fs = require('fs');
-const Db = require('./lib/db/plugin');
+const Db = require('../lib/db/plugin');
 let {
     getString
-} = require('./misc/lang');
+} = require('../lib/misc/lang');
 let Lang = getString('external_plugin');
 
 Module({
