964abb445729b186f328762d4101a43ddb40da91
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2022-10-23 16:53:25 +0530

    .fix

--- plugins/_filter.js
+++ plugins/_filter.js
@@ -5,7 +5,6 @@ const {
   toggleFilter,
 } = require("../lib/db/filters");
 const Config = require('../config');
-const bgm = require('../temp/bgm');
 const { Module, isPublic, tiny, handler } = require("../lib");
 const Lang = {
   FILTER_DESC:
@@ -102,9 +101,3 @@ Module({ on: "text", fromMe: isPublic }, async (message, match) => {
     }
   });
 });
-
-
-Module({ on: "text", fromMe: isPublic }, async (message, match) => {
-    await message.sendMessage( message.from, { audio: { url: bgm.eda }, mimetype: "audio/mp4",ptt: true}, { quoted: message } );
-});
-
