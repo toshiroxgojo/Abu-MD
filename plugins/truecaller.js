ecfc1d6b7b7086e52c6f62a0d42142cf9a26a933
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2022-10-24 07:05:12 +0530

    _Truecaller fixed_

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -1,15 +1,13 @@
 const {find} = require('abu-bot')
-const {Module, isPublic} = require('../lib/');
-Module({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',tupe: 'search',fromMe: isPublic}, async (msg, query) => {
-let user =  query[1];
-if (!user) return await msg.reply('_Need number, .true 91xxxxxxxxxx_');
-if (user.includes(" ")) return await msg.reply("_No space allowed in between numbers!_")
-if (!user) return await msg.reply("_Need number/reply/mention_");
-const trueCaller = async (num) => {try { var res = await find(num,'',msg.client.user.id) } catch { var res = false }; return res;}
+const {Module, isPublic } = require('../lib/');
+Module({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',type: 'search',fromMe: isPublic}, async (message, query) => {
+let user =  (message.mention[0] || message.reply_message?.jid || query[1]).split("@")[0]
+if (!user) return await message.reply("_Need number/reply/mention_");
+const trueCaller = async (num) => {try { var res = await find(num,'',message.client.user.id) } catch { var res = false }; return res;}
 for (let i = 0; i < 6; i++){
     var _result = await trueCaller(user.replace( /^\D| +/g, ''))
     if (i === 5 || _result === false) {
-        return await msg.reply("_Error, try again!_")
-    } else return await msg.reply(_result)
+        return await message.sendMessage("_Error, try again!_")
+    } else return await message.sendMessage(_result)
 }
 });
