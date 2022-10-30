53c6c2d97ec17010eb399f19f8ad47a18915ea17
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2022-10-30 08:51:07 +0530

    .fix

---
+++ plugins/eval.js
@@ -0,0 +1,53 @@
+const {
+  Function,
+  isPublic,
+  getUrl,
+  fromBuffer,
+  Imgur,
+  getBuffer,
+  getJson,
+  Fancy,
+  AddMp3Meta,
+  formatBytes,
+  parseJid,
+  isUrl,
+  parsedJid,
+  pinterest,
+  wallpaper,
+  wikimedia,
+  quotesAnime,
+  aiovideodl,
+  umma,
+  ringtone,
+  styletext,
+  FileSize,
+  h2k,
+  textpro,
+  yt,
+  ytIdRegex,
+  yta,
+  ytv,
+  runtime,
+  clockString,
+  sleep,
+  jsonformat,
+  Serialize,
+  processTime,
+  Module,
+} = require("../lib/");
+const util = require("util");
+const config = require("../config");
+
+
+Module({pattern:'eval', on: "text", fromMe: true,desc :'Runs a server code'}, async (message, match, m, client) => {
+  if (match.startsWith(">")) {
+    //const m = message;
+    try {
+      let evaled = await eval(`${match.replace(">", "")}`);
+      if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+      await message.reply(evaled);
+    } catch (err) {
+      await message.reply(util.format(err));
+    }
+  }
+});
