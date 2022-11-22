170174b0c096086c3cc7ca2f40fc96f03c708698
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2022-11-22 08:00:54 +0530

    fix run time

--- plugins/uptime.js
+++ plugins/uptime.js
@@ -39,11 +39,10 @@ Module({
   ut_hour = ut_hour%60; 
   ut_min = ut_min%60; 
   ut_sec = ut_sec%60; 
-  var uptime_os = (`_System (OS) : ${ut_hour} Hour(s), ${ut_min} minute(s) and ${ut_sec} second(s)_`)  
   var sec_num = parseInt(process.uptime(), 10);
   var hours   = Math.floor(sec_num / 3600);
   var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
   var seconds = sec_num - (hours * 3600) - (minutes * 60);
-  var uptime_process = (`_Process : ${hours} Hour(s), ${minutes} minute(s) and ${seconds} second(s)_`)  
+  var uptime_process = (`_Runtime : ${hours} Hour(s), ${minutes} minute(s) and ${seconds} second(s)_`)  
   return await message.reply(`${uptime_process}`);
 }));
