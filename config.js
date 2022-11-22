1acc25c0764e62b7cc2abd456bec5f71b503087f
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2022-11-22 07:51:27 +0530

    fix

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ module.exports = {
   GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
   AUTHOR: process.env.AUTHOR || "Jsl",
   DATABASE: DATABASE_URL === './database.db' ? new Sequelize({ dialect: 'sqlite', storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
-  BOT_INFO: process.env.BOT_INFO || 'Abu MD,Jsl,Abu SER,Abu MD,𝑭𝑬𝑨𝑹 𝑭𝑬𝑨𝑹 👀🤍,𝑬𝑵𝑵𝑨 𝑴𝑾𝑶𝑵𝑼 𝑷𝑬𝑨𝑹 𝑨𝒀𝑰 💗,https://wa.me/917025994178,https://telegra.ph/file/47842cf7d85784cb4e441.jpg',
+  BOT_INFO: process.env.BOT_INFO || 'Abu MD,Jsl,Abu SER,Abu MD,𝑭𝑬𝑨𝑹 𝑭𝑬𝑨𝑹 👀🤍,𝑬𝑵𝑵𝑨 𝑴𝑾𝑶𝑵𝑼 𝑷𝑬𝑨𝑹 𝑨𝒀𝑰 💗,https://wa.me/917025994178,https://telegra.ph/file/47842cf7d85784cb4e441.jpg,https://i.imgur.com/z5NM5d3.jpeg',
   SUDO: process.env.SUDO || "917025994178",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || " ",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || " ",
