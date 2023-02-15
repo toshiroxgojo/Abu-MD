f0a9584ade38cb7eacacd185ac1b4f8a090665a5
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2023-02-15 08:59:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -31,5 +31,4 @@ module.exports = {
   WELCOME_MSG: process.env.WELCOME_MSG || "Hi @user Welcome to @gname",
   GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
   AUTHOR: process.env.AUTHOR || "Jsl",
- DATABASE: DATABASE_URL === "./database.db" ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: "postgres", ssl: true, protocol: "postgres", dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),l
-};
+ };
