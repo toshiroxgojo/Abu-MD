a3eb706d12a93ebd18abcc6fc37060454ce8fc29
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2023-02-15 08:33:45 +0530

    test

---
+++ plugins/ping.js
@@ -0,0 +1,17 @@
+const { Jsl ,isPublic } = require("../lib/");
+Jsl(
+  {
+    pattern: "ping",
+    fromMe: isPublic,
+    desc: "To check ping",
+    type: "user",
+  },
+  async (message, match) => {
+    const start = new Date().getTime();
+    await message.reply("*_Testing Bot Speed..._*");
+    const end = new Date().getTime();
+    return await message.reply(
+      "*_Response in" + (end - start) + "ms_*"
+    );
+  }
+);
