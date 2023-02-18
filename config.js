362c02b803871521e9a573298b38dc7ad1a5ece9
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2023-02-18 07:57:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ module.exports = {
   HANDLERS:
     process.env.HANDLER === "false" || process.env.HANDLER === "null"
       ? "^"
-      : "^[,]",
+      : "false",
   RMBG_KEY: process.env.RMBG_KEY || false,
   BRANCH: "main",
   PACKNAME: process.env.PACKNAME || "Abu,💔",
