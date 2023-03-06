d9ae52870a6f80114a1d4eaa2dfa4d8b74d5ee5e
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2023-03-06 07:58:22 +0530

    ux

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,17 +1,18 @@
 const { Module,isPublic } = require("../lib/");
+const { FancyRandom } = require('abu-bot');
 Module(
   {
-    pattern: "ping",
+    pattern: "rt",
     fromMe: isPublic,
-    desc: "To check ping",
+    desc: "check bot ping",
     type: "user",
   },
   async (message, match) => {
     const start = new Date().getTime();
-    await message.reply("*_Testing Bot Speed..._*");
-    const end = new Date().getTime();
-    return await message.reply(
-      "*_Response in" + (end - start) + "ms_*"
-    );
+    const botz = await FancyRandom("Testing Bot Speed...⌫")
+    await message.reply(botz);
+    const end = new Date().getTime();    
+const Jsl1 = await FancyRandom("✿︎..Response in " + (end - start) + " ms..✿︎")
+    return await message.reply(Jsl1);
   }
 );
